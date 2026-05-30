1bba7ca2d32fd4afd675c694e7b7e10cd8b4b621
Patryk Z. <znamirowskipatryk@gmail.com>
2026-05-30 16:18:05 +0200

    Fix: Vercel deployment

--- apps/web/src/app/modules/Storage/useCurrentUserStore.ts
+++ apps/web/src/app/modules/Storage/useCurrentUserStore.ts
@@ -1,19 +1,19 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-
-type CurrentUserStore = {
-  currentUser: CurrentUserResponse["data"] | undefined;
-  setCurrentUser: (value: CurrentUserStore["currentUser"]) => void;
-};
-
-export const useCurrentUserStore = create<CurrentUserStore>()(
-  persist(
-    (set) => ({
-      currentUser: undefined,
-      setCurrentUser: (value) => set({ currentUser: value }),
-    }),
-    {
-      name: "current-user-storage",
-    },
-  ),
-);
+//
+// type CurrentUserStore = {
+//   currentUser: CurrentUserResponse["data"] | undefined;
+//   setCurrentUser: (value: CurrentUserStore["currentUser"]) => void;
+// };
+//
+// export const useCurrentUserStore = create<CurrentUserStore>()(
+//   persist(
+//     (set) => ({
+//       currentUser: undefined,
+//       setCurrentUser: (value) => set({ currentUser: value }),
+//     }),
+//     {
+//       name: "current-user-storage",
+//     },
+//   ),
+// );
