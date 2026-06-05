e2cb41d18c0918241da2d625fcaa47bb8dda7e81
Patryk Z. <znamirowskipatryk@gmail.com>
2026-06-05 17:56:17 +0200

    Fix: Commented unused code for deployment

--- apps/web/src/app/api/queries/useProjects.ts
+++ apps/web/src/app/api/queries/useProjects.ts
@@ -1,20 +1,20 @@
-import { useQuery } from "@tanstack/react-query";
-import { ApiClient } from "@/app/api/client.ts";
-import type { GetProjectResponse } from "@/app/api/generated-api.ts";
-
-export const useProjectList = () => {
-  useQuery({
-    queryKey: ["projects"],
-    queryFn: () => ApiClient.projects.projectsControllerGetProjects(),
-    select: (response): GetProjectResponse => response.data,
-  });
-};
-
-export const useProject = (projectUuid: string) => {
-  useQuery({
-    queryKey: ["projects", projectUuid],
-    queryFn: () =>
-      ApiClient.projects.projectsControllerGetProjectById(projectUuid),
-    select: (response): GetProjectResponse => response.data,
-  });
-};
+// import { useQuery } from "@tanstack/react-query";
+// import { ApiClient } from "@/app/api/client.ts";
+// import type { GetProjectResponse } from "@/app/api/generated-api.ts";
+//
+// export const useProjectList = () => {
+//   useQuery({
+//     queryKey: ["projects"],
+//     queryFn: () => ApiClient.projects.projectsControllerGetProjects(),
+//     select: (response): GetProjectResponse => response.data,
+//   });
+// };
+//
+// export const useProject = (projectUuid: string) => {
+//   useQuery({
+//     queryKey: ["projects", projectUuid],
+//     queryFn: () =>
+//       ApiClient.projects.projectsControllerGetProjectById(projectUuid),
+//     select: (response): GetProjectResponse => response.data,
+//   });
+// };
